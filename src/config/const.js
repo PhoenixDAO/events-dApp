644f552c1ba9a9d8368aa23e4a69fac7d126e7cb
JafferAbbas-codes <beastincarnate121@gmail.com>
2021-03-23 15:33:51 +0500

    fixed active events section

--- src/config/const.js
+++ src/config/const.js
@@ -2,7 +2,7 @@
 //Mainnet
 // export const INFURA_URL="https://mainnet.infura.io/v3/c89f216154d84b83bb9344a7d0a91108"
 // export const INFURA_WEB_URL="wss://mainnet.infura.io/ws/v3/c89f216154d84b83bb9344a7d0a91108"
-// export const API_URL = "https://phoenix-dao-event-dapp.herokuapp.com/";
+// export const API_URL = "http://localhost:4000/";
 // export const GLOBAL_NETWORK_ID=1;
 // export const REPORT_EVENT = "event/report";
 
@@ -11,7 +11,6 @@
 //Rinkeby
 export const INFURA_URL="https://rinkeby.infura.io/v3/c89f216154d84b83bb9344a7d0a91108"
 export const INFURA_WEB_URL="wss://rinkeby.infura.io/ws/v3/72e114745bbf4822b987489c119f858b"
-
 export const API_URL = "https://phoenix-dao-event-dapp.herokuapp.com/";
 export const GLOBAL_NETWORK_ID=4;
 export const REPORT_EVENT = "event/report";
