69a4ad8c0d32714738ec08cb87a9761b5b85d0ea
faheelsattar <faheelsattar@yahoo.com>
2021-07-16 10:09:50 +0500

    added the option for avatar 🦄

--- src/components/account/detailform.jsx
+++ src/components/account/detailform.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./detailform.css";
 import CreateOutlinedIcon from "@material-ui/icons/CreateOutlined";
+import DialogueBox from "../common/DialogueBox";
+import roundlogo from "../Images/roundlogo.svg";
+
 const DetailForm = () => {
+	const [open, setOpen] = useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const avatars = [
+		{ img: "/images/metamask.svg", name: "Bennue" },
+		{ img: "/images/metamask.svg", name: "Bennue" },
+		{ img: "/images/metamask.svg", name: "Bennue" },
+		{ img: "/images/metamask.svg", name: "Bennue" },
+		{ img: "/images/metamask.svg", name: "Bennue" },
+		{ img: "/images/metamask.svg", name: "Bennue" },
+	].map((data) => {
+		return (
+			<div className="single-avatar-hldr">
+				<div className="acc-av-hldr">
+					<img className="acc-av" src={data.img} />
+				</div>
+				<div className="acc-title-hlder">
+					<p className="acc-title"> {data.name} </p>
+				</div>
+			</div>
+		);
+	});
 	return (
 		<div className="dtl-hldr">
 			<div className="acc-basic-info">
@@ -10,7 +42,10 @@ const DetailForm = () => {
 				</div>
 				<div className="acc-title-hlder">
 					<p className="acc-title"> Bennu </p>
-					<CreateOutlinedIcon style={{ color: "blueviolet", width:"20px" }}  />
+					<CreateOutlinedIcon
+						onClick={handleOpen}
+						style={{ color: "blueviolet", width: "20px" }}
+					/>
 				</div>
 			</div>
 			<div className="acc-form-hldr">
@@ -57,6 +92,32 @@ const DetailForm = () => {
 					</div>
 				</form>
 			</div>
+			<DialogueBox open={open} handleClose={handleClose}>
+				<div className="idn-hldr">
+					<div className="idn-head">
+						<div>
+							<img
+								style={{ height: "24px" }}
+								src={roundlogo}
+								alt="phnx logo"
+							/>
+						</div>
+						<div>
+							<p className="idn-heading">PhoenixDAO</p>
+						</div>
+					</div>
+					<div className="idn-sub">
+						<h5 className="idn-subhead1"> Choose your identity</h5>
+						<p className="idn-subhead2">
+							This will be used as your avatar in the dApp
+						</p>
+					</div>
+					<div className="avatar-hldr">{avatars}</div>
+					<div className="">
+						<button className="avatar-select-btn">Select</button>
+					</div>
+				</div>
+			</DialogueBox>
 		</div>
 	);
 };
