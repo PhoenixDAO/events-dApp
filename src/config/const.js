54e0cb246672e054cdace94da7630d949c25fb3f
Musfirazia <42489563+Musfirazia@users.noreply.github.com>
2021-09-16 10:47:01 +0500

    ethereum contract integrated

--- src/config/const.js
+++ src/config/const.js
@@ -38,10 +38,12 @@
 // export const graphURL="https://api.thegraph.com/subgraphs/name/mudassir45/events-dapp2"
 
 //Rinkeby	
-export const INFURA_URL =
-	"https://rinkeby.infura.io/v3/388c2e54484b4d90a0a54aa9238f1db7";
-export const INFURA_WEB_URL =
-	"wss://rinkeby.infura.io/ws/v3/388c2e54484b4d90a0a54aa9238f1db7";
+export const INFURA_URL="https://mainnet.infura.io/v3/3c62e62341b34568a512b8917a59ad63"
+export const INFURA_WEB_URL="wss://mainnet.infura.io/ws/v3/3c62e62341b34568a512b8917a59ad63"
+// export const INFURA_URL =
+// 	"https://rinkeby.infura.io/v3/388c2e54484b4d90a0a54aa9238f1db7";
+// export const INFURA_WEB_URL =
+	// "wss://rinkeby.infura.io/ws/v3/388c2e54484b4d90a0a54aa9238f1db7";
 export const INFURA_URL_2="https://speedy-nodes-nyc.moralis.io/27dcd9d4dd0ceef1ad3788eb/polygon/mainnet";
 export const INFURA_WEB_URL_2="wss://speedy-nodes-nyc.moralis.io/27dcd9d4dd0ceef1ad3788eb/polygon/mainnet/ws";
 // export const INFURA_URL_2 =
@@ -49,7 +51,7 @@ export const INFURA_WEB_URL_2="wss://speedy-nodes-nyc.moralis.io/27dcd9d4dd0ceef
 // export const INFURA_WEB_URL_2 =
 // 	"wss://goerli.infura.io/ws/v3/388c2e54484b4d90a0a54aa9238f1db7";
 
-export const GLOBAL_NETWORK_ID = 4;
+export const GLOBAL_NETWORK_ID = 1;
 export const GLOBAL_NETWORK_ID_2 = 137;
 
 export const API_URL = "https://events-api.phoenixdao.io/";
@@ -65,8 +67,10 @@ export const UPDATE_EVENT_VIEWS = "user/updateViews";
 export const GET_MESSAGE = "user/getMessage";
 export const LOGIN_METAMASK = "user/loginUserWithMetamask";
 export const TWEET = "user/tweet";
-export const explorerWithTX = "https://rinkeby.etherscan.io/tx/";
-export const explorerWithAddress = "https://rinkeby.etherscan.io/address/";
+export const explorerWithTX="https://explorer-mainnet.maticvigil.com/tx/"
+export const explorerWithAddress ="https://explorer-mainnet.maticvigil.com/address/"
+// export const explorerWithTX = "https://rinkeby.etherscan.io/tx/";
+// export const explorerWithAddress = "https://rinkeby.etherscan.io/address/";
 // export const graphURL =
 // 	"https://api.thegraph.com/subgraphs/name/nashaibakbar/eventcheck";
 //for rinkeby
