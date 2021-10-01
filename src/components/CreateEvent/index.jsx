c116ef817a1dae4035771ffd9ad2355e043fe051
Musfirazia <42489563+Musfirazia@users.noreply.github.com>
2021-10-01 16:00:35 +0500

    slider text improved

--- src/components/CreateEvent/index.jsx
+++ src/components/CreateEvent/index.jsx
@@ -708,7 +708,6 @@ class CreateEvent extends Component {
 					<div className="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-xs-12">
 						<br />
 						<br />
-						<br />
 						<MyStepper
 							handleCreateEvent={this.handleCreateEvent}
 							onFieldsChange={this.onFieldsChange}
